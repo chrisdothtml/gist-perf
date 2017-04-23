dbf9030d67cccca3b18da9ef351b8c79e3bf9207
Chris Deacy <chrisdothtml@gmail.com>
2017-04-22 19:32:48 -0500

    adding router as mixin for all tags

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import { mount } from 'riot'
+import Navigo from 'navigo'
+import { mixin, mount } from 'riot'
 import './tags/app/app.tag'
 import './index.styl'
 
+// add router to all tags
+mixin({ router: new Navigo(null, true, '#!') })
 mount('app')
