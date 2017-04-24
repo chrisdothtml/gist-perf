54acd59dac7d92b8a20724449608e454d4280d02
Chris Deacy <chrisdothtml@gmail.com>
2017-04-24 08:36:49 -0500

    adding maintenance mode in case someone, somehow is able to view the live site

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import Navigo from 'navigo'
 import { mixin, mount } from 'riot'
-import './tags/app/app.tag'
+import './tags/maintenance/maintenance.tag'
 import './index.styl'
 
 const router = new Navigo(null, true, '#!')
 
 mixin('router', { router })
-mount('app')
+mount('maintenance')
---
+++ src/tags/maintenance/maintenance.tag
@@ -0,0 +1,10 @@
+<maintenance>
+  <div class="maintenance">
+    <h1>Offline for maintenance</h1>
+    <p>This app is undergoing maintenance right now.<br/>Please check back later.</p>
+  </div>
+
+  <script>
+    import './maintenance.styl'
+  </script>
+</maintenance>
